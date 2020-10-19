06e3fd83d4c8606465f85235a481831189c83fcd
kara-b <dyon@dyonb.nl>
2020-10-19 21:08:30 +0200

    Some extra info about commands

--- src/commands/other.rs
+++ src/commands/other.rs
@@ -6,6 +6,8 @@ use serenity::framework::standard::{
 };
 
 #[command]
+#[description = "Pong!"]
+#[aliases("pong")]
 async fn ping(ctx: &Context, message: &Message) -> CommandResult {
     message.channel_id.say(&ctx.http, "Pong!").await?;
 
@@ -13,6 +15,8 @@ async fn ping(ctx: &Context, message: &Message) -> CommandResult {
 }
 
 #[command]
+#[description = "Some information about the bot."]
+#[aliases("info")]
 async fn about(ctx: &Context, message: &Message) -> CommandResult {
     let avatar_url = ctx.cache.current_user().await.avatar_url();
 
@@ -21,7 +25,7 @@ async fn about(ctx: &Context, message: &Message) -> CommandResult {
             e.title("About kBot");
             e.description(format!("**Bot source**\n{}\n**Support server**\n{}",
                                   "https://github.com/kara-b/kBot2", "https://discord.gg/qzGj4En"));
-            e.thumbnail(avatar_url.unwrap_or(String::from("")));
+            e.thumbnail(avatar_url.unwrap_or_else(String::new));
             e
         });
         m
@@ -29,12 +33,3 @@ async fn about(ctx: &Context, message: &Message) -> CommandResult {
 
     Ok(())
 }
-
-// commandContext.replyEmbedBlocking(embedCreateSpec ->
-// embedCreateSpec.setTitle("About kBot")
-// .setDescription("**Bot source**\n" +
-// kBot.config.bot_source + "\n" +
-// "**Support server**\n" +
-// kBot.config.supportServer + "\n")
-// .setThumbnail(commandContext.getBotMember().getAvatarUrl())
-// .setColor(kBotUtilities.randomBrightColor()));
