0546c9b216402cffdb933106acf7f1e74cf68cd2
dyon <dyon@dyonb.nl>
2020-11-11 12:02:16 +0100

    Get rid of from implementation

--- src/models/modrinth.rs
+++ src/models/modrinth.rs
@@ -26,27 +26,6 @@ pub struct SearchedModrinthMod {
     pub host: String,
 }
 
-impl From<FullModrinthMod> for SearchedModrinthMod {
-    fn from(full_mod: FullModrinthMod) -> Self {
-        Self {
-            mod_id: full_mod.id,
-            author: "author".to_string(),
-            title: full_mod.title,
-            description: full_mod.description,
-            categories: full_mod.categories,
-            versions: full_mod.versions,
-            downloads: full_mod.downloads,
-            page_url: "page_url".to_string(),
-            icon_url: full_mod.icon_url,
-            author_url: "author_url".to_string(),
-            date_created: full_mod.published,
-            date_modified: full_mod.updated,
-            latest_version: "latest_version".to_string(),
-            host: "host".to_string()
-        }
-    }
-}
-
 #[derive(Deserialize, Debug)]
 pub struct FullModrinthMod {
     pub id: String,
