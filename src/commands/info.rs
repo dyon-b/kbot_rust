b75f3e8238fe5973720bbc510cb090c1d1e99850
dyon <dyon@dyonb.nl>
2020-11-13 09:38:44 +0100

    From 800ms to 300ms

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -4,19 +4,20 @@ use serenity::framework::standard::{
     CommandResult,
     macros::command,
 };
-use serenity::futures::{StreamExt};
+use std::time::Instant;
 
 #[command]
 #[description = "Gives information about a guild"]
 #[only_in("guilds")]
 #[aliases("server", "guild", "guildinfo")]
 async fn serverinfo(ctx: &Context, msg: &Message) -> CommandResult {
+    let cached_guild = msg.guild_id.unwrap().to_guild_cached(&ctx.cache).await.unwrap();
+
+    // Collect the channel count from cache to be speedy
     let mut text_channels = 0;
     let mut voice_channels = 0;
 
-    // Collect the channel count
-    let channels = msg.guild_id.unwrap().channels(ctx).await?;
-    for channel in channels {
+    for channel in &cached_guild.channels {
         let channel = channel.1;
         if channel.kind == ChannelType::Text {
             text_channels += 1;
@@ -25,32 +26,26 @@ async fn serverinfo(ctx: &Context, msg: &Message) -> CommandResult {
         }
     }
 
+    // Collect the member count
     let mut bot_count = 0;
     let mut human_count = 0;
 
-    // Collect the member count
-    let mut members_stream = msg.guild_id.unwrap().members_iter(&ctx).boxed();
-    while let Some(member_result) = members_stream.next().await {
-        match member_result {
-            Ok(member) => {
-                if member.user.bot { bot_count += 1 } else { human_count += 1 }
-            },
-            Err(why) => eprintln!("An error occurred when iterating through members: {}", why),
-        }
+    for member_result in &cached_guild.members {
+        if member_result.1.user.bot { bot_count += 1 } else { human_count += 1 };
     }
     let member_count = bot_count + human_count;
 
-    let guild = msg.guild(ctx).await.unwrap();
-    let owner: User = guild.owner_id.to_user(ctx).await?;
+    // Get the guild owner
+    let owner: User = cached_guild.owner_id.to_user(&ctx).await?;
 
     msg.channel_id.send_message(ctx, |m| m.embed(|e| {
-        e.title(format!("Server info for {}", guild.name))
-            .thumbnail(guild.icon_url().unwrap())
+        e.title(format!("Server info for {}", cached_guild.name))
+            .thumbnail(cached_guild.icon_url().unwrap())
             .field("Owner", format!("{}#{}", owner.name, owner.discriminator), true)
-            .field("Region", &guild.region, true)
+            .field("Region", &cached_guild.region, true)
             .field("Channels", format!("**Text:** {}\n**Voice:** {}\n", text_channels, voice_channels), true)
             .field("Members", format!("**Total:** {}\n**Humans:** {}\n**Bots:** {}\n", member_count, human_count, bot_count), true)
-            .footer(|f| f.text(format!("ID: {}", guild.id.0)))
+            .footer(|f| f.text(format!("ID: {}", cached_guild.id.0)))
     })).await?;
 
     Ok(())
